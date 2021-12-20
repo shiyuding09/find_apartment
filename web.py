2a355edd4baa3659ade3a0b930e1bcac2e8b49d2
shiyuding09 <96407187+shiyuding09@users.noreply.github.com>
2021-12-20 04:12:24 -0500

    Update web.py

--- web.py
+++ web.py
@@ -32,19 +32,14 @@ def poi_plot(num):
 @app.route("/get_question/<str1>",methods=['GET'])
 def get_question(str1):
     array_list=json.loads(str1)
-    array_list.append(0)
     temp_tree=tree
     for i in array_list:
         temp_tree=temp_tree[i]
-    if isinstance(temp_tree,str):
-        return temp_tree,200
-    if isinstance(temp_tree,int):
-        array_list.pop(-1)
-        for i in array_list:
-            temp_tree = temp_tree[i]
-        apt_selected_list=[apt_list['num'][i] for i in temp_tree]
+    if isinstance(temp_tree,tuple):
+        return temp_tree[0],200
+    if isinstance(temp_tree,list):
         list_total=list(range(315))
-        apt_selected_list=list(set(list_total).difference(set(apt_selected_list)))
+        apt_selected_list=list(set(list_total).difference(set(temp_tree)))
         apt_selected_list={"num":apt_selected_list}
         return json.dumps(apt_selected_list),300
 
@@ -52,10 +47,3 @@ def get_question(str1):
 
 if __name__=='__main__':
     app.run(port=8080,debug=True)
-
-
-
-
-
-
-
